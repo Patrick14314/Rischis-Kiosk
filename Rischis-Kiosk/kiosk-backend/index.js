1592f00c3670babd6bbc846670e21c9ad6bf3db2
Patrick14314 <tz48@proton.me>
2025-06-07 01:46:47 +0200

    Fix Supabase env variable

--- Rischis-Kiosk/kiosk-backend/index.js
+++ Rischis-Kiosk/kiosk-backend/index.js
@@ -44,7 +44,7 @@ app.use('/feed', require('./routes/feed'));
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE
 );
 
 app.get('/auth/me', async (req, res) => {
