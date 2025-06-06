08a864c597c95e412ce6a3e7bd2347c3b759f4b7
Patrick14314 <tz48@proton.me>
2025-06-07 00:54:26 +0200

    Update supabase.js

--- Rischis-Kiosk/kiosk-backend/supabase.js
+++ Rischis-Kiosk/kiosk-backend/supabase.js
@@ -1,8 +1,9 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE;
+const supabaseKey = process.env.SUPABASE_ANON_KEY; // <-- Richtiger Key!
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
+
