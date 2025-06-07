aa1bd391c32d0d8ca41f8f289001ff8cf143509d
Patrick14314 <tz48@proton.me>
2025-06-07 15:16:14 +0200

    Delete Rischis-Kiosk directory

--- Rischis-Kiosk/kiosk-backend/supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
