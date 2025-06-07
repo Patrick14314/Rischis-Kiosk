a88c5460cf15c8ec840e81d842349c196c3e072f
Patrick14314 <tz48@proton.me>
2025-06-07 15:50:54 +0200

    Enable cookie parsing for auth

--- kiosk-backend/index.js
+++ kiosk-backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Static routes for HTML entry points
